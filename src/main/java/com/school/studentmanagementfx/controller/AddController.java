56304454bc8c94d89bc6aff88f46ab3f67d69596
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-06 20:32:50 +0800

    feat: bind input fields in AddView to controller for student data entry

--- src/main/java/com/school/studentmanagementfx/controller/AddController.java
+++ src/main/java/com/school/studentmanagementfx/controller/AddController.java
@@ -3,9 +3,34 @@ package com.school.studentmanagementfx.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AddController {
+
+    @FXML
+    TextField idTextField;
+
+    @FXML
+    TextField nameTextField;
+
+    @FXML
+    TextField ageTextField;
+
+    @FXML
+    TextField birthdayTextField;
+
+    @FXML
+    TextField addressTextField;
+
+    @FXML
+    TextField courseTextField;
+
+    @FXML
+    TextField yearTextField;
+
+    @FXML
+    TextField emailTextField;
 
     @FXML
     private void cancelButton(ActionEvent event) {
