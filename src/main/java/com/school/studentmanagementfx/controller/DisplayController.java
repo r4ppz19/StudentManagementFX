a2c03dde0a44a89771250bb126295267d8698aef
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-08 22:00:55 +0800

    feat: refactor table column configuration into ConfigureTable helper class

--- src/main/java/com/school/studentmanagementfx/controller/DisplayController.java
+++ src/main/java/com/school/studentmanagementfx/controller/DisplayController.java
@@ -1,5 +1,6 @@
 package com.school.studentmanagementfx.controller;
 
+import com.school.studentmanagementfx.helper.ConfigureTable;
 import com.school.studentmanagementfx.model.Student;
 import com.school.studentmanagementfx.repository.StudentRepository;
 import javafx.fxml.FXML;
@@ -29,36 +30,8 @@ public class DisplayController {
 
     @FXML
     public void initialize() {
-        tableView.setEditable(false);
-
-        configureColumn(idColumn);
-        configureColumn(nameColumn);
-        configureColumn(ageColumn);
-        configureColumn(birthdayColumn);
-        configureColumn(addressColumn);
-        configureColumn(courseColumn);
-        configureColumn(yearColumn);
-        configureColumn(emailColumn);
-
-        populateTable(idColumn, nameColumn, ageColumn, birthdayColumn, addressColumn, courseColumn, yearColumn, emailColumn);
-
+        ConfigureTable.configureTableColumns(idColumn, nameColumn, ageColumn, birthdayColumn, addressColumn,
+                courseColumn, yearColumn, emailColumn);
         tableView.setItems(StudentRepository.getStudents());
-    }
-
-    static void populateTable(TableColumn<Student, String> idColumn, TableColumn<Student, String> nameColumn, TableColumn<Student, String> ageColumn, TableColumn<Student, String> birthdayColumn, TableColumn<Student, String> addressColumn, TableColumn<Student, String> courseColumn, TableColumn<Student, String> yearColumn, TableColumn<Student, String> emailColumn) {
-        idColumn.setCellValueFactory(cellData -> cellData.getValue().getId());
-        nameColumn.setCellValueFactory(cellData -> cellData.getValue().getName());
-        ageColumn.setCellValueFactory(cellData -> cellData.getValue().getAge());
-        birthdayColumn.setCellValueFactory(cellData -> cellData.getValue().getBirthday());
-        addressColumn.setCellValueFactory(cellData -> cellData.getValue().getAddress());
-        courseColumn.setCellValueFactory(cellData -> cellData.getValue().getCourse());
-        yearColumn.setCellValueFactory(cellData -> cellData.getValue().getYear());
-        emailColumn.setCellValueFactory(cellData -> cellData.getValue().getEmail());
-    }
-
-    private void configureColumn(TableColumn<Student, String> column) {
-        column.setReorderable(false);
-        column.setSortable(false);
-        column.setResizable(false);
     }
 }
