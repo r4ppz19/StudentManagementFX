1848c47d598096a5739b1730e6bef3a399dd821f
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-09 13:13:49 +0800

    feat: enhance AddController to validate input fields and display error modal

--- src/main/java/com/school/studentmanagementfx/controller/AddController.java
+++ src/main/java/com/school/studentmanagementfx/controller/AddController.java
@@ -1,10 +1,14 @@
 package com.school.studentmanagementfx.controller;
 
+import com.school.studentmanagementfx.helper.CreateWindow;
 import com.school.studentmanagementfx.model.Student;
 import com.school.studentmanagementfx.repository.StudentRepository;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class AddController {
     @FXML
@@ -25,21 +29,40 @@ public class AddController {
     private TextField emailTextField;
 
     @FXML
-    private void addBtn() {
-        StudentRepository.getStudents().add(getInfo());
+    private void addBtn(ActionEvent event) throws IOException {
+        StudentRepository.getStudents().add(getInfo(event));
         clearFields();
     }
 
-    private Student getInfo() {
-        String name = nameTextField.getText().trim();
-        String address = addressTextField.getText().trim();
-        String course = courseTextField.getText().trim();
-        String email = emailTextField.getText().trim();
-        String birthday = birthdayTextField.getText().trim();
-        String age = ageTextField.getText().trim();
-        String year = yearTextField.getText().trim();
-        String id = idTextField.getText().trim();
-        return new Student(id, name, age, birthday, address, course, year, email);
+    private Student getInfo(ActionEvent event) throws IOException {
+        String name = nameTextField.getText();
+        String address = addressTextField.getText();
+        String course = courseTextField.getText();
+        String email = emailTextField.getText();
+        String birthday = birthdayTextField.getText();
+        String age = ageTextField.getText();
+        String year = yearTextField.getText();
+        String id = idTextField.getText();
+
+        if (isNullOrEmpty(name) && isNullOrEmpty(address) && isNullOrEmpty(course) && isNullOrEmpty(email)
+                && isNullOrEmpty(birthday) && isNullOrEmpty(age) && isNullOrEmpty(year) && isNullOrEmpty(id)) {
+            String errorViewFxml = "/com/school/studentmanagementfx/view/ErrorView.fxml";
+            CreateWindow.createModalWindow(event, errorViewFxml);
+        }
+
+        return new Student(
+                id.trim(),
+                name.trim(),
+                age.trim(),
+                birthday.trim(),
+                address.trim(),
+                course.trim(),
+                year.trim(),
+                email.trim());
+    }
+
+    private boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
     }
 
     private void clearFields() {
