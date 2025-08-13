327a8666211072d213602262d8ce613f139abb7c
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-13 19:19:47 +0800

    feat: add modal window for adding students
    Implemented a new AddStudentView.fxml for the add student modal and updated HomePageController to open this modal using CreateWindow. This provides a UI for entering student details such as ID, name, age, birthday, address, course, year, and email.

--- src/main/java/com/school/studentmanagementfx/controller/HomePageController.java
+++ src/main/java/com/school/studentmanagementfx/controller/HomePageController.java
@@ -1,7 +1,11 @@
 package com.school.studentmanagementfx.controller;
 
+import com.school.studentmanagementfx.helper.CreateWindow;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class HomePageController {
 
@@ -14,7 +18,8 @@ public class HomePageController {
     }
 
     @FXML
-    private void onAddStudentAction() {
-
+    private void onAddStudentAction(ActionEvent  event) throws IOException {
+        String addStudentFxml = "/com/school/studentmanagementfx/view/AddStudentView.fxml";
+        CreateWindow.createModalWindow(event, addStudentFxml);
     }
 }
