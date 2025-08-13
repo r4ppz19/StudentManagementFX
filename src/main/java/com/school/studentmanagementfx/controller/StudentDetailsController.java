55c5c0fcf9a0bd81654c44a3e26556ab350051ff
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-13 19:59:47 +0800

    feat: add student details view and controller
    Introduced StudentDetailsController Java class and corresponding StudentDetailsView.fxml layout for displaying student information in a read-only form. The view includes fields for ID, name, age, birthday, address, course, year, and email, along with a Close button.

---
+++ src/main/java/com/school/studentmanagementfx/controller/StudentDetailsController.java
@@ -0,0 +1,30 @@
+package com.school.studentmanagementfx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
+public class StudentDetailsController {
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField ageTextField;
+    @FXML
+    private TextField birthdayTextField;
+    @FXML
+    private TextField addressTextField;
+    @FXML
+    private TextField courseTextField;
+    @FXML
+    private TextField yearTextField;
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private void onCloseAction(ActionEvent event) {
+
+    }
+}
