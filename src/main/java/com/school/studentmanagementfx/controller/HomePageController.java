5656721d420f56790e88edeb93c55a4cba7637a1
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-13 18:33:10 +0800

    feat: add HomePage UI with controller and styles
    Introduced HomePageController.java, HomePageView.fxml for the student management system UI, and TableView.css for custom table styling.

---
+++ src/main/java/com/school/studentmanagementfx/controller/HomePageController.java
@@ -0,0 +1,20 @@
+package com.school.studentmanagementfx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class HomePageController {
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private void onSearchStudentAction() {
+
+    }
+
+    @FXML
+    private void onAddStudentAction() {
+
+    }
+}
