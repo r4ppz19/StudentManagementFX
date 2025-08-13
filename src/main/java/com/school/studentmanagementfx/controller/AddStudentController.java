ebea78467c85817bc1e2edd88f7107098ffcf864
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-13 19:28:00 +0800

    feat: add id and eventhandler methods to the controller class

---
+++ src/main/java/com/school/studentmanagementfx/controller/AddStudentController.java
@@ -0,0 +1,34 @@
+package com.school.studentmanagementfx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddStudentController {
+
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField ageTextField;
+    @FXML
+    private TextField birthdayTextField;
+    @FXML
+    private TextField addressTextField;
+    @FXML
+    private TextField courseTextField;
+    @FXML
+    private TextField yearTextField;
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private void onCancelAction() {
+
+    }
+
+    @FXML
+    private void onAddStudentAction() {
+    
+    }
+}
