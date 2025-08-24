83c4a4bbf42de47cc1b59acd212f4fa50d681344
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-24 19:17:24 +0800

    feat: add icons
    Added icon font library in pom and update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,16 @@
 module com.school.studentmanagementfx {
+    // JavaFX
+    requires javafx.base;
     requires javafx.graphics;
-    requires transitive javafx.controls;
-    requires transitive javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    // IconFonts
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign2;
 
     opens com.school.studentmanagementfx to javafx.fxml, javafx.graphics;
     opens com.school.studentmanagementfx.controller to javafx.fxml;
-    opens com.school.studentmanagementfx.model to javafx.base;
     opens com.school.studentmanagementfx.view to javafx.fxml;
 }
